ad663be0aac870dbb741e4319f96ff481822588b
Mayaka Kobayashi <amacrk@icloud.com>
2022-12-04 10:20:14 +0900

    Initial Commit

---
+++ PhotoShow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoShow
+//
+//  Created by maya on 2022/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
